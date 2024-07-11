b0dc746f24229ec06747e20eb648f3effe88de11
ThuanTVT <vanthuansgu@gmai.com>
2024-07-11 15:59:25 +0700

    update sort

--- Lab/Lab3/sort_array.js
+++ Lab/Lab3/sort_array.js
@@ -1,8 +1,22 @@
 let intArr = [12, 34, 1, 16, 28];
 
-function sortArray(arr) {
-    return arr.sort((a, b) => a - b);
+function selectionSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        // Tìm phần tử nhỏ nhất trong mảng chưa được sắp xếp
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // Hoán đổi phần tử nhỏ nhất với phần tử đầu tiên
+        let temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
 }
 
-let sortedArr = sortArray(intArr);
-console.log(`Sorted array: ${sortedArr}`);
+let sortedArr = selectionSort(intArr);
+console.log(`Mảng đã sắp xếp: ${sortedArr}`);
